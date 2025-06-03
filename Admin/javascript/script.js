7630ff2f54c3e0fe367ff907135b3f41b560d3fd
Quynh16vna <quynh16vna@gmail.com>
2025-06-03 20:40:22 +0700

    Bổ sung thư viện, icon, ảnh

---
+++ Admin/javascript/script.js
@@ -0,0 +1,13 @@
+﻿
+$(function () {
+    // this will get the full URL at the address bar
+    var url = window.location.href;
+
+    // passes on every "a" tag 
+    $("#sub-header a").each(function () {
+        // checks if its the same on the address bar
+        if (url == (this.href)) {
+            $(this).closest("a").addClass("active");
+        }
+    });
+});
